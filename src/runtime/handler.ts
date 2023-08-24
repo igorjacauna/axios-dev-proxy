f668dae06ceb82d6c3f8c5ebdb825988df241a12
Igor Jacaúna <igor.jacauna@gmail.com>
2023-08-24 12:54:30 -0400

    refactor: Create handler class

---
+++ src/runtime/handler.ts
@@ -0,0 +1,139 @@
+import type { AxiosResponse } from 'axios';
+import {
+  ejectFromRequest,
+  ejectFromResponse,
+  matchRequest,
+  matchResponse,
+} from './helpers';
+import type Proxy from './proxy';
+
+export default class Handler {
+  scope!: Proxy;
+
+  verb!: string;
+
+  path!: string;
+
+  params?: object;
+
+  constructor(scope: Proxy, verb: string, path: string, params?: object) {
+    this.scope = scope;
+    this.verb = verb;
+    this.path = path;
+    this.params = params;
+  }
+
+  private setProxy(
+    statusCodeOrFunction: number | RouteConfig,
+    mock?: unknown,
+    once = false,
+  ) {
+    const interceptorId = this.scope.axios.interceptors.request.use(
+      requestConfig => {
+        if (matchRequest(this.verb, this.path, requestConfig, this.params)) {
+          requestConfig.adapter = config => {
+            if (once) ejectFromRequest(this.scope.axios, interceptorId);
+            return new Promise((resolve, reject) => {
+              if (typeof statusCodeOrFunction === 'function') {
+                Promise.resolve(statusCodeOrFunction(requestConfig)).then(
+                  result => {
+                    const [status, data] = result;
+                    const response = {
+                      data,
+                      status,
+                      headers: {},
+                      statusText: '',
+                      config,
+                      request: requestConfig,
+                    };
+                    if (status < 400) resolve(response);
+                    else reject(response);
+                  },
+                );
+                return;
+              }
+              const status = statusCodeOrFunction;
+              const response: AxiosResponse = {
+                data: mock,
+                status,
+                headers: {},
+                statusText: '',
+                config,
+                request: requestConfig,
+              };
+
+              if (status < 400) resolve(response);
+              else reject(response);
+            });
+          };
+        }
+        return requestConfig;
+      },
+    );
+  }
+
+  private setRequestConfigChanger(
+    configChanger: RequestConfigChanger,
+    once = false,
+  ) {
+    const verbConfig = this.verb;
+    const pathConfig = this.path;
+    const paramsConfig = this.params;
+    const interceptorId = this.scope.axios.interceptors.request.use(
+      requestConfig => {
+        if (matchRequest(verbConfig, pathConfig, requestConfig, paramsConfig)) {
+          if (once) ejectFromRequest(this.scope.axios, interceptorId);
+          const result = configChanger(requestConfig);
+          return result;
+        }
+        return requestConfig;
+      },
+    );
+  }
+
+  private setPrintableResponse(once = false) {
+    const verbConfig = this.verb;
+    const pathConfig = this.path;
+    const paramsConfig = this.params;
+    const interceptorId = this.scope.axios.interceptors.response.use(
+      response => {
+        if (matchResponse(verbConfig, pathConfig, response, paramsConfig)) {
+          if (once) ejectFromResponse(this.scope.axios, interceptorId);
+          console.log('Response from:', this.path);
+          console.log(JSON.stringify(response.data, null, 2));
+        }
+        return response;
+      },
+    );
+  }
+
+  reply(statusCodeOrConfig: number | RouteConfig, mock?: unknown) {
+    this.setProxy(statusCodeOrConfig, mock);
+    return this.scope;
+  }
+
+  replyOnce(statusCodeOrConfig: number | RouteConfig, mock?: unknown) {
+    this.setProxy(statusCodeOrConfig, mock, true);
+    return this.scope;
+  }
+
+  changeRequest(changer: RequestConfigChanger) {
+    this.setRequestConfigChanger(changer);
+    return this;
+  }
+
+  changeRequestOnce(changer: RequestConfigChanger) {
+    this.setRequestConfigChanger(changer, true);
+    return this;
+  }
+
+  printResponse() {
+    this.setPrintableResponse();
+    return this;
+  }
+
+  printResponseOnce() {
+    this.setPrintableResponse(true);
+    return this;
+  }
+}
--- src/runtime/proxy.ts
+++ src/runtime/proxy.ts
@@ -1,11 +1,6 @@
-import type { AxiosResponse, AxiosInstance } from 'axios';
-import {
-  clearAll,
-  ejectFromRequest,
-  ejectFromResponse,
-  matchRequest,
-  matchResponse,
-} from './helpers';
+import type { AxiosInstance } from 'axios';
+import Handler from './handler';
+import { clearAll } from './helpers';
 
 export default class Proxy {
   axios: AxiosInstance;
@@ -16,132 +11,17 @@ export default class Proxy {
 
   params?: object;
 
-  once: boolean;
-
   constructor(axios: AxiosInstance) {
     this.axios = axios;
-    this.once = false;
-  }
-
-  private setProxy(statusCodeOrFunction: number | RouteConfig, mock?: unknown) {
-    const verbConfig = this.verb;
-    const pathConfig = this.path;
-    const onceConfig = this.once;
-    const paramsConfig = this.params;
-    const interceptorId = this.axios.interceptors.request.use(requestConfig => {
-      if (matchRequest(verbConfig, pathConfig, requestConfig, paramsConfig)) {
-        requestConfig.adapter = config => {
-          if (onceConfig) ejectFromRequest(this.axios, interceptorId);
-          return new Promise((resolve, reject) => {
-            if (typeof statusCodeOrFunction === 'function') {
-              Promise.resolve(statusCodeOrFunction(requestConfig)).then(
-                result => {
-                  const [status, data] = result;
-                  const response = {
-                    data,
-                    status,
-                    headers: {},
-                    statusText: '',
-                    config,
-                    request: requestConfig,
-                  };
-                  if (status < 400) resolve(response);
-                  else reject(response);
-                },
-              );
-              return;
-            }
-            const status = statusCodeOrFunction;
-            const response: AxiosResponse = {
-              data: mock,
-              status,
-              headers: {},
-              statusText: '',
-              config,
-              request: requestConfig,
-            };
-
-            if (status < 400) resolve(response);
-            else reject(response);
-          });
-        };
-      }
-      return requestConfig;
-    });
-  }
-
-  private setRequestConfigChanger(
-    configChanger: RequestConfigChanger,
-    once = false,
-  ) {
-    const verbConfig = this.verb;
-    const pathConfig = this.path;
-    const paramsConfig = this.params;
-    const interceptorId = this.axios.interceptors.request.use(requestConfig => {
-      if (matchRequest(verbConfig, pathConfig, requestConfig, paramsConfig)) {
-        if (once) ejectFromRequest(this.axios, interceptorId);
-        const result = configChanger(requestConfig);
-        return result;
-      }
-      return requestConfig;
-    });
-  }
-
-  private setPrintableResponse(once = false) {
-    const verbConfig = this.verb;
-    const pathConfig = this.path;
-    const paramsConfig = this.params;
-    const interceptorId = this.axios.interceptors.response.use(response => {
-      if (matchResponse(verbConfig, pathConfig, response, paramsConfig)) {
-        if (once) ejectFromResponse(this.axios, interceptorId);
-        console.log('Response from:', this.path);
-        console.log(JSON.stringify(response.data, null, 2));
-      }
-      return response;
-    });
   }
 
   private setup(verb: string, path: string, params?: object) {
-    this.verb = verb;
-    this.path = path;
-    this.params = params;
-    return this;
+    const handler = new Handler(this, verb, path, params);
+    return handler;
   }
 
   clear() {
     clearAll(this.axios);
-  }
-
-  reply(statusCodeOrConfig: number | RouteConfig, mock?: unknown) {
-    this.once = false;
-    this.setProxy(statusCodeOrConfig, mock);
-    return this;
-  }
-
-  replyOnce(statusCodeOrConfig: number | RouteConfig, mock?: unknown) {
-    this.once = true;
-    this.setProxy(statusCodeOrConfig, mock);
-    return this;
-  }
-
-  changeRequest(changer: RequestConfigChanger) {
-    this.setRequestConfigChanger(changer);
-    return this;
-  }
-
-  changeRequestOnce(changer: RequestConfigChanger) {
-    this.setRequestConfigChanger(changer, true);
-    return this;
-  }
-
-  printResponse() {
-    this.setPrintableResponse();
-    return this;
-  }
-
-  printResponseOnce() {
-    this.setPrintableResponse(true);
-    return this;
   }
 
   onGet(path: string, params?: object) {
